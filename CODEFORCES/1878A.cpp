d1aa19ed01a4cb54976794075920378382fd800f
ZuHaYer Dev HEEMU <145164413+zuhayerdevheemu333@users.noreply.github.com>
2023-09-26 23:24:34 +0600

    CFX

---
+++ CODEFORCES/1878A.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ZDH                     ios_base
+#define ll                      long long
+#define db                      double
+#define str                     string
+#define pb                      push_back
+#define pf                      push_front
+#define MIN                     INT_MIN
+#define MAX                     INT_MAX
+#define sz                      size()
+#define C(X)                    cout << X << endl
+#define all(exe)                exe.begin(), exe.end()
+#define f(exe)                  for (int i = 0; i < exe; i++)
+
+void sync()
+{
+    ZDH::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+}
+
+void solution()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    bool mark = false;
+    for (int i = 0; i < n; ++i)
+    {
+        if (v[i] == k)
+        {
+            mark = true;
+            break;
+        }
+    }
+    if (mark)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int main()
+{
+    sync();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
